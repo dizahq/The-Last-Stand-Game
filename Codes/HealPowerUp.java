9bcf3151968e1d6f4b1071c3cc71c265d02c732f
MattL <mdlaraga@up.edu.ph>
2026-05-03 22:03:44 +0800

    added sprite

--- Codes/HealPowerUp.java
+++ Codes/HealPowerUp.java
@@ -2,16 +2,28 @@ package Codes;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
 public class HealPowerUp extends Powerup{
+    private Image icon;
+
     public HealPowerUp(int x, int y){
         super(x, y);
         setDuration(0);
+
+        icon = new ImageIcon("Entities/Object/PowerUps/heal.png").getImage();
     }
     @Override
     public void draw(Graphics g) {
-        g.setColor(Color.GREEN);
-        g.fillOval(x, y, width, height);
+        if (icon != null && icon.getWidth(null) != 1){
+            g.drawImage(icon, x, y, width, height, null);
+        }else{
+            //if image fails to load
+            g.setColor(Color.GREEN);
+            g.fillOval(x, y, width, height);
+        }
+        
     }
     @Override
     public void applyEffect(Player player) {
