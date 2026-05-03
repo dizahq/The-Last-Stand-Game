a0ae3b1e7f50cbbdace85d66807f0f5d436501e7
MattL <mdlaraga@up.edu.ph>
2026-05-03 22:04:10 +0800

    Added sprite

--- Codes/MovementSpeedPowerup.java
+++ Codes/MovementSpeedPowerup.java
@@ -2,17 +2,30 @@ package Codes;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
 public class MovementSpeedPowerup extends Powerup{
+    private Image icon;
+
     public MovementSpeedPowerup(int x, int y){
         super(x, y);
         setDuration(10000);
+
+        icon = new ImageIcon("Entities/Object/PowerUps/speedboost.png").getImage();
     }
+
     @Override
     public void draw(Graphics g) {
-        g.setColor(Color.BLUE);
-        g.fillOval(x, y, width, height);
+        if (icon != null && icon.getWidth(null) != 1){
+            g.drawImage(icon, x, y, width, height, null);
+        }else{
+            //if image fails to load
+            g.setColor(Color.BLUE);
+            g.fillOval(x, y, width, height);
+        }
     }
+
     @Override
     public void applyEffect(Player player) {
         player.setSpeed(5);
