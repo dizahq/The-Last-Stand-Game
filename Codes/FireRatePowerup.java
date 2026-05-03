d6222c8623dd9c878959a6cb0c832f0ef574cfd1
MattL <mdlaraga@up.edu.ph>
2026-05-03 22:03:19 +0800

    Added sprite

--- Codes/FireRatePowerup.java
+++ Codes/FireRatePowerup.java
@@ -2,16 +2,27 @@ package Codes;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
 public class FireRatePowerup extends Powerup{
+    private Image icon;
+
     public FireRatePowerup(int x, int y){
         super(x, y);
         setDuration(5000);
+
+        icon = new ImageIcon("Entities/Object/PowerUps/atkspeed.png").getImage();
     }
     @Override
     public void draw(Graphics g) {
-        g.setColor(Color.RED);
-        g.fillOval(x, y, width, height);
+        if (icon != null && icon.getWidth(null) != 1){
+            g.drawImage(icon, x, y, width, height, null);
+        }else{
+            //if image fails to load
+            g.setColor(Color.RED);
+            g.fillOval(x, y, width, height);
+        }
     }
     @Override
     public void applyEffect(Player player) {
