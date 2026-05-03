e7240cfac17933e463f2a82971729ef0ef427076
MattL <mdlaraga@up.edu.ph>
2026-05-03 21:59:38 +0800

    Fixed directions & Added sprites

--- Codes/Bullet.java
+++ Codes/Bullet.java
@@ -3,6 +3,8 @@ package Codes;
 import java.awt.Graphics;
 import java.awt.Image;
 
+import javax.swing.ImageIcon;
+
 public class Bullet extends Entity{
     private Direction direction; //direction of where the bullet must travel
     private int speed;
@@ -11,59 +13,58 @@ public class Bullet extends Entity{
     private static Image[] bulletImages;
 
     public Bullet(int x, int y, Direction direction){
-        super(x, y, 20, 20);
+        super(x, y, 35, 35);
         this.direction = direction;
-        this.speed = -10;
+        this.speed = 10;
 
-        /* Load images once, observes the order based on the Direction enum
+        // Load images once, observes the order based on the Direction enum
         if (bulletImages == null) {
             bulletImages = new Image[]{
-                new ImageIcon("Entities/Bullet/bullet_north.png").getImage(),
-                new ImageIcon("Entities/Bullet/bullet_northeast.png").getImage(),
-                new ImageIcon("Entities/Bullet/bullet_east.png").getImage(),
-                new ImageIcon("Entities/Bullet/bullet_southeast.png").getImage(),
-                new ImageIcon("Entities/Bullet/bullet_south.png").getImage(),
-                new ImageIcon("Entities/Bullet/bullet_southwest.png").getImage(),
-                new ImageIcon("Entities/Bullet/bullet_west.png").getImage(),
-                new ImageIcon("Entities/Bullet/bullet_northwest.png").getImage()
+                new ImageIcon("Entities/Object/ammo/arrow_up.png").getImage(),
+                new ImageIcon("Entities/Object/ammo/arrow_upRight.png").getImage(),
+                new ImageIcon("Entities/Object/ammo/arrow_right.png").getImage(),
+                new ImageIcon("Entities/Object/ammo/arrow_downRight.png").getImage(),
+                new ImageIcon("Entities/Object/ammo/arrow_down.png").getImage(),
+                new ImageIcon("Entities/Object/ammo/arrow_downLeft.png").getImage(),
+                new ImageIcon("Entities/Object/ammo/arrow_left.png").getImage(),
+                new ImageIcon("Entities/Object/ammo/arrow_upLeft.png").getImage()
             };
         }
-        */
 
         //Maps directly to the correct image in the array (ordinal returns the enum position)
-        //currentImage = bulletImages[direction.ordinal()];
+        currentImage = bulletImages[direction.ordinal()];
     }
 
     //Moves the bullet each game tick based on its direction
     public void update(){
         switch (direction) {
             case NORTH:
-                this.y += speed;
+                this.y -= speed;
                 break;
             case NORTHEAST:
                 this.x += speed;
-                this.y += speed;
+                this.y -= speed;
                 break;
             case EAST:
                 this.x += speed;
                 break;
             case SOUTHEAST:
                 this.x += speed;
-                this.y -= speed;
+                this.y += speed;
                 break;
             case SOUTH:
-                this.y -= speed;
+                this.y += speed;
                 break;
             case SOUTHWEST:
                 this.x -= speed;
-                this.y -= speed;
+                this.y += speed;
                 break;
             case WEST:
                 this.x -= speed;
                 break;
             case NORTHWEST:
                 this.x -= speed;
-                this.y += speed;
+                this.y -= speed;
                 break;
             default:
                 break;
